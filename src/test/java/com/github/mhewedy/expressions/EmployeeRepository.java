804cca109bd826107f49d27add2baf2ef23a56ae
mhewedy <mhewedy@users.noreply.github.com>
2021-09-17 14:08:24 +0000

    deploy: ea08a26ba7384b7ad0721aba4ad87ed0d7b1f73e

--- src/test/java/com/github/mhewedy/expressions/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.mhewedy.expressions;
-
-import com.github.mhewedy.expressions.model.Employee;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends ExpressionsRepository<Employee, Long> {
-}
--- src/test/java/com/github/mhewedy/expressions/model/Status.java
+++
@@ -1,5 +0,0 @@
-package com.github.mhewedy.expressions.model;
-
-public enum Status {
-    ACTIVE, NOT_ACTIVE
-}
